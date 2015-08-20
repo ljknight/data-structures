c7d219a4418b5fc7dcaf91e8601ba9074454f2be
ljknight <ljknight@gmail.com>
2015-08-19 22:25:39 -0700

    Added partial functionality for stack on prototypal pattern.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+
+  cLog: function() { console.log(this.storage); },
+
+  push: function(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  },
+
+  pop: function() {
+    var value = this.storage[Object.keys(this.storage).length-1];
+    console.log(this.storage[Object.keys(this.storage).length-1]);
+    delete this.storage[Object.keys(this.storage).length-1];
+    
+    return value; 
+  },
+
+  size: function() {
+    // console.log(Object.keys(this.storage).length);
+    return Object.keys(this.storage).length;
+  }
+};
 
 
