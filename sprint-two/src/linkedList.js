333777b5d29bef2666c9b4f9fa7c5e318dd9bf92
ljknight <ljknight@gmail.com>
2015-08-20 11:40:55 -0700

    Partially implemented LinkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    // var prevTail = this.tail || newNode;
+
+    if( this.head === undefined ){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode; // {}
+      this.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    // reassign head to current head's next
+    // return list.head
+    var currentHead = list.head;
+    var newHead = list.head.next || list.head;
+    if(list.head )
+    list.head = newHead;
+    return currentHead;
   };
 
   list.contains = function(target){
+    // check all values, return boolean
   };
 
   return list;
