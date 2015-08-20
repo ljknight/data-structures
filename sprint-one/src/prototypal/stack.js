940d6bbeb85344d734a79d83b287d568a21b3f20
ljknight <ljknight@gmail.com>
2015-08-20 09:45:00 -0700

    Completed stack for prototypal pattern.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,13 +3,12 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(stackMethods);
 
+  someInstance.storage = {};
+
   return someInstance;
 };
 
 var stackMethods = {
-  storage: {},
-
-  cLog: function() { console.log(this.storage); },
 
   push: function(value) {
     this.storage[Object.keys(this.storage).length] = value;
