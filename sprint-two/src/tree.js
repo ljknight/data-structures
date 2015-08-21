27489b4777579f2ed826b3b43223c2c329efa3f5
ljknight <ljknight@gmail.com>
2015-08-21 10:56:08 -0700

    Implemented tree using functional-shared pattern.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,34 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-  // your code here
-  newTree.children = null;  // fix me
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
-
+  var contained = false;
+  var childChecker = function(tree){
+    if( tree.value === target ){
+      contained = true;
+      return;
+    }
+    if( tree.children.length > 0 ){
+      for( var i = 0; i < tree.children.length; i++ ){
+        childChecker(tree.children[i]);
+      }
+    }
+    return contained;
+  }
+  return childChecker(this);
 };
 
 
